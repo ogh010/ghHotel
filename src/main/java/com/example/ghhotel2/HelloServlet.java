d8a44533037e98abfa2534363d30240f461f11e7
오가희 <72989911+ogh010@users.noreply.github.com>
2025-06-07 17:53:47 +0900

    버전

--- src/main/java/com/example/ghhotel2/HelloServlet.java
+++ src/main/java/com/example/ghhotel2/HelloServlet.java
@@ -1,15 +1,13 @@
 package com.example.ghhotel2;
 
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-
-
-
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
